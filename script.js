6ced9358522666c8a207dca194436e95056c43f7
JordyDuran <jordyduran2000@gmail.com>
2024-09-28 20:21:28 -0600

    cambios fondo, texto de boda y nombres

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el bloque de texto de la fecha
+    const fecha = document.querySelector('.fecha');
+
+    // Función para verificar si el elemento está en el viewport
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Agrega el evento scroll para verificar cuando el elemento está en pantalla
+    window.addEventListener('scroll', function () {
+        if (isInViewport(fecha)) {
+            fecha.classList.add('visible'); // Añade la clase "visible" cuando el elemento es visible
+        }
+    });
+});
+
+
 const frase = "Para siempre es un río que fluye sin detenerse y sin embargo permanece - Mario Benedetti";
 const fraseContainer = document.getElementById("frase-benedetti");
 
@@ -18,3 +42,6 @@ palabras.forEach((palabra, index) => {
         fraseContainer.appendChild(br);
     }
 });
+
+
+
